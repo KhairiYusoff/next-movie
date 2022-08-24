4b9312f28d6866cacec2e05317f624230847cb86
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-08-24 19:54:47 +0800

    Create new pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { RiSearchLine } from "react-icons/ri";
 
 const Container = styled.div`
 background: #333333;
@@ -22,17 +23,49 @@ justify-content: space-between;
 align-items: center;
 `
 
-const MenuItems = styled.div`
-padding: 0 20px;
+const MenuItems = styled.a`
+position: relative;
+letter-spacing: 1.5px;
+text-decoration: none;
+padding:0 20px;
+cursor: pointer;
+&:after{
+    content: "";
+    position: absolute;
+    background-color: #FED530;
+    height: 4px;
+    width: 0;
+    left: 0;
+    bottom: -10px;
+    transition: 0.5s;
+}
+&:hover::after{
+  width: 100%;
+}
 `
 
-const Right = styled.div``
+const Right = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 10px;
+`
 
-const RightItems = styled.div``
+const RightItems = styled.div`
+margin-left: 15px;
+`
 
-const RightCircle = styled.div``
+const RightCircle = styled.div`
+width: 48px;
+height: 48px;
+background: #1E1E1E;
+border-radius: 50%;
+margin-left: 15px;
+`
 
-
+const SearchIcon = styled.div`
+cursor: pointer;
+`
 const Navbar = () => {
     return (
         <Container>
@@ -50,7 +83,9 @@ const Navbar = () => {
                     <MenuItems>Contact Us</MenuItems>
                 </Menu>
                 <Right>
-                    <RightItems></RightItems>
+                    <SearchIcon>
+                        <RiSearchLine size={20} />
+                    </SearchIcon>
                     <RightCircle></RightCircle>
                     <RightItems>John Glich</RightItems>
                 </Right>
